76a698cda619b699407a9a3a654748d72131e119
pavan.n <pavan.n@thekaios.com>
2025-12-16 12:15:35 +0530

    fixed

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
 func main() {
 	input := " 42 "
+	p := strings.TrimSpace(input)
 
-	ans, err := strconv.Atoi(input) 
+	ans, err := strconv.Atoi(p)
 	_ = err
 
 	fmt.Println(ans)
 }
-
